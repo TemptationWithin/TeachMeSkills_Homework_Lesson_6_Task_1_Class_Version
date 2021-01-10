15a73417ce59af63e31201001fe75228d10d23d2
TemptationWithin <75949800+TemptationWithin@users.noreply.github.com>
2021-01-10 13:59:17 +0300

    Add files via upload

---
+++ src/com/homeWork_Lesson_6_Task_1_Class_Version/exceptions/DivisionByZeroException.java
@@ -0,0 +1,7 @@
+package com.homeWork_Lesson_6_Task_1_Class_Version.exceptions;
+
+public class DivisionByZeroException extends ArithmeticException{
+    public DivisionByZeroException(String message){
+        super(message);
+    }
+}
